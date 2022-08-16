a2103e9fe65857e4f7a6d2c45afa62fb54bb783e
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-08-16 15:12:39 +0300

    Migrate to Java 11 and Eclipse Temurin based Docker image

--- start-application.sh
+++ start-application.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 if [[ "${DEBUG_ENABLED}" = true ]]; then
-  java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar /usr/app/mqtt-pulsar-gateway.jar
+  java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -XX:InitialRAMPercentage=10.0 -XX:MaxRAMPercentage=95.0 -jar /usr/app/mqtt-pulsar-gateway.jar
 else
-  java -jar /usr/app/mqtt-pulsar-gateway.jar
+  java -XX:InitialRAMPercentage=10.0 -XX:MaxRAMPercentage=95.0 -jar /usr/app/mqtt-pulsar-gateway.jar
 fi
