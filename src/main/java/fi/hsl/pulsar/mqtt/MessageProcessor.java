0031a8951c5b9c2cd934e6cf77b18b46b225270d
Sebastian Szeszko <se.szeszko@gmail.com>
2025-09-03 08:43:17 +0200

    feat: 60301 structured logging poc
    (cherry picked from commit abb49316576fdfa577f52a1fcc7f47ff35c07388)

--- src/main/java/fi/hsl/pulsar/mqtt/MessageProcessor.java
+++ src/main/java/fi/hsl/pulsar/mqtt/MessageProcessor.java
@@ -4,6 +4,14 @@ import com.typesafe.config.Config;
 import fi.hsl.common.pulsar.PulsarApplication;
 import fi.hsl.common.transitdata.TransitdataProperties;
 import fi.hsl.pulsar.mqtt.utils.BusyWait;
+import fi.hsl.pulsar.mqtt.utils.LogUtils;
+import org.apache.pulsar.client.api.MessageId;
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.TypedMessageBuilder;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.HashMap;
@@ -14,12 +22,6 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiFunction;
-import org.apache.pulsar.client.api.MessageId;
-import org.apache.pulsar.client.api.Producer;
-import org.apache.pulsar.client.api.TypedMessageBuilder;
-import org.eclipse.paho.client.mqttv3.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MessageProcessor implements IMqttMessageHandler {
     private static final Logger log = LoggerFactory.getLogger(MessageProcessor.class);
@@ -111,7 +113,10 @@ public class MessageProcessor implements IMqttMessageHandler {
             if (inFlight < 0 || inFlight > IN_FLIGHT_ALERT_THRESHOLD) {
                 log.error("Pulsar insert cannot keep up with the MQTT feed! In flight: {}", inFlight);
             } else {
-                log.info("Currently messages in flight: {}", inFlight);
+                Map<String, String> logFields = Map.of(
+                        "inFlightCount", String.valueOf(inFlight)
+                );
+                LogUtils.withFields(logFields, () -> log.info("In flight messages count"));
             }
         }
     }
