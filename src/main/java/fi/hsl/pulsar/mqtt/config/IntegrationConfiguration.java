926171809a4b88503d70582c53b4e79d9e38db34
haphut <haphut@mistmap.com>
2026-04-22 23:12:22 +0300

    refactor: remove unused @IntegrationComponentScan

--- src/main/java/fi/hsl/pulsar/mqtt/config/IntegrationConfiguration.java
+++ src/main/java/fi/hsl/pulsar/mqtt/config/IntegrationConfiguration.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
@@ -19,7 +18,6 @@ import org.springframework.messaging.MessageHandler;
 
 @Configuration
 @EnableIntegration
-@IntegrationComponentScan
 public class IntegrationConfiguration {
 
     private static final Logger log = LoggerFactory.getLogger(IntegrationConfiguration.class);
